bc8673dac2e70177fab8d1d6ba002327e144a93b
unknown <jbielecki18@gmail.com>
2020-12-15 17:02:52 +0100

    add lazy loading (suspense and lazy)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Switch
 } from 'react-router-dom';
 
-import Draws from './pages/Draws';
-import Dashboard from './pages/Dashboard';
-import NotFound from './pages/NotFound';
+import Spinner from './components/Spinner';
+
+const Draws = lazy(() => import('./pages/Draws'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const NotFound = lazy(() => import('./pages/NotFound'));
 
 const App = () => (
   <Router>
+    <Suspense fallback={<Spinner />}>
       <Switch>
           <Route exact path="/" component={Draws} />
           <Route exact path="/dashboard" component={Dashboard} />
           <Route path="*" component={NotFound} />
       </Switch>
+    </Suspense>
   </Router>
 );
 
